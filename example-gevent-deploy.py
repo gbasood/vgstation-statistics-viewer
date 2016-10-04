dfd408ce1a95e2b97100f9d136dc54911f25d6dc
Sood <gbasood@gmail.com>
2016-10-04 10:27:28 -0700

    Fixes url_for hopefully

--- example-gevent-deploy.py
+++ example-gevent-deploy.py
@@ -1,7 +1,5 @@
 from gevent.pywsgi import WSGIServer
 from app import app
 
-app.config['SERVER_NAME'] = 'http://game.ss13.moe/stats'
-
 http_server = WSGIServer(('', 8080), app)
 http_server.serve_forever()
