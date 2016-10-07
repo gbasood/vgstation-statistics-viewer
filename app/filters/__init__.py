49b3fbe3eae5e5673ff84bcb07ed1b081e5eadbe
Sood <gbasood@gmail.com>
2016-10-07 08:53:16 -0700

    Fixed round times showing up incorrectly

--- app/filters/__init__.py
+++ app/filters/__init__.py
@@ -7,10 +7,10 @@ def format_timestamp(value, format='matchtime'):
     if format == 'matchtime':
         # yyyy mm dd hh mm ss
         value = value.encode('ascii').split('.')
-        return "{} {} {}:{}".format(calendar.month_name[int(value[1])], int(value[3]), int(value[3]), value[4] )
+        return "{} {} {}:{}".format(calendar.month_name[int(value[1])], int(value[2]), int(value[3]), value[4] )
     if format == 'shortmatchtime':
         value = value.encode('ascii').split('.')
-        return "{}/{} {}:{}".format(int(value[1]), int(value[3]), int(value[3]), value[4] )
+        return "{}/{} {}:{}".format(int(value[1]), int(value[2]), int(value[3]), value[4] )
 
 @app.template_filter('obj_successfail')
 def obj_successfail(succeeded):
