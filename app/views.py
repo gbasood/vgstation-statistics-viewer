c6a15b9ece26c89417bec10aeb1637c25004ba4d
Sood <gbasood@gmail.com>
2016-10-08 21:05:45 -0700

    500 page

--- app/views.py
+++ app/views.py
@@ -54,3 +54,8 @@ def alert_new_file():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+@app.route('/error')
+def errorpage():
+    return render_template('500.html'), 500
