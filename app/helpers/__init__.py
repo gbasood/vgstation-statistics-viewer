a0840d2a5648aacb985a6fb892981bb6173ac832
Sood <gbasood@gmail.com>
2016-10-08 21:16:13 -0700

    why is this even still here

--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -5,9 +5,9 @@ from app import app, db, models
 def modethumb(name):
     name=name.lower()
     if os.path.isfile(os.path.join(basedir, 'app','static','img','modethumbs', name + '.png')):
-        return flask.url_for('static', filename='img/modethumbs/' + name + '.png', external=True)
+        return flask.url_for('static', filename='img/modethumbs/' + name + '.png')
     else:
-        return flask.url_for('static', filename='img/modethumbs/othermode.png', external=True)
+        return flask.url_for('static', filename='img/modethumbs/othermode.png')
 
 app.jinja_env.globals.update(modethumb=modethumb)
 
