1d9afe6fd7b4acf197b85d05e002912e84a0df0a
Sood <gbasood@gmail.com>
2016-10-28 12:13:22 -0700

    Fix tests and factories

--- testcontent/factory/match/__init__.py
+++ testcontent/factory/match/__init__.py
@@ -1,13 +1,23 @@
-import factory
+import factory, datetime, pytz
+from datetime import tzinfo
 from app import models, db
 from testcontent import factory as ourfactory
-from factory.fuzzy import FuzzyInteger, FuzzyChoice
+from factory.fuzzy import FuzzyInteger, FuzzyChoice, FuzzyDateTime
+
+def filename():
+    parsed_file_date = FuzzyDateTime(datetime.datetime(2012, 1, 1, tzinfo=pytz.timezone('US/Eastern')))
+    parsed_file = "statistics_{}".format(parsed_file_date.evaluate(2, None, False).strftime('%Y.%m.%d.%Y%m%d'))
+    return parsed_file
+
 
 class MatchFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = models.Match
         sqlalchemy_session = db.session
-    # modes_string = FuzzyChoice('cult', 'revolution', 'vampire', 'autotraitor', 'nuclear emergency', 'blob', 'wizard', 'ragin\' mages', 'ai malfunction', 'revolutionary squad', 'changeling')
-    modes_string = FuzzyChoice('cult')
-    if modes_string === 'cult':
-        cultstat = factory.RelatedFactory(ourfactory.CultStatFactory, 'match_id')
+    # modes_string = FuzzyChoice(['cult', 'revolution', 'vampire', 'autotraitor', 'nuclear emergency', 'blob', 'wizard', 'ragin\' mages', 'ai malfunction', 'revolutionary squad', 'changeling'])
+    id = factory.Sequence(lambda n: n)
+    modes_string = FuzzyChoice(['cult']).fuzz()
+    parsed_file = filename()
+    if modes_string == 'cult':
+        cultstat = factory.SubFactory(ourfactory.cult.CultStatFactory)
+    #to be continued as factories are added
