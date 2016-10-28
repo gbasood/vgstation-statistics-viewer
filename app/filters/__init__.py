c961239c3444e349a1e72a819dfc703c6db9b197
PJB3005 <pieterjan.briers@gmail.com>
2016-10-28 21:50:20 +0200

    Whoops forgot to commit this

--- app/filters/__init__.py
+++ app/filters/__init__.py
@@ -2,18 +2,20 @@ from datetime import date, datetime
 from app import app, models
 import calendar
 
+
 @app.template_filter('format_timestamp')
 def format_timestamp(value, format='matchtime'):
     if format == 'matchtime':
         # yyyy mm dd hh mm ss
         value = value.encode('ascii').split('.')
-        return "{} {} {}:{}".format(calendar.month_name[int(value[1])], int(value[2]), int(value[3]), value[4] )
+        return "{} {} {}:{}".format(calendar.month_name[int(value[1])], int(value[2]), int(value[3]), value[4])
     elif format == 'shortmatchtime':
         value = value.encode('ascii').split('.')
-        return "{}/{} {}:{}".format(int(value[1]), int(value[2]), int(value[3]), value[4] )
-    elif format == 'hhmm': # datetime hour/min
+        return "{}/{} {}:{}".format(int(value[1]), int(value[2]), int(value[3]), value[4])
+    elif format == 'hhmm':  # datetime hour/min
         value = value.encode('ascii').split('.')
         return "{}:{}".format(value[4], value[5])
+
 
 @app.template_filter('obj_successfail')
 def obj_successfail(succeeded):
@@ -21,6 +23,7 @@ def obj_successfail(succeeded):
         return "<span class='objective success'>Success</span>"
     else:
         return "<span class='objective failure'>Failure</span>"
+
 
 @app.template_filter('obj_pretty')
 def obj_pretty(objective):
