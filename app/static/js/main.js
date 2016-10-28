aa5c8bb52d72cfc624cd7a234a5c54676f45b4ed
Sood <gbasood@gmail.com>
2016-10-28 09:49:38 -0700

    Current progress

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,17 +3,17 @@ window.onload = function() {
     var matches = document.getElementsByClassName('match')
     for(var i = 0; i < matches.length; i++) {
         var match = matches[i];
-        match.addEventListener("click", match_clickthrough, false);
+        match.onclick=match_clickthrough, match
     }
 }
 
-function match_clickthrough() {
+function match_clickthrough(match) {
   var destination = '/match/';
-  if(event.target.dataset.matchid !== undefined) {
-    destination += event.target.dataset.matchid;
+  if(match.target.dataset.matchid !== undefined) {
+    destination += match.target.dataset.matchid;
   } else {
-    console.log(event.target);
-    var parent = event.target.parentElement;
+    console.log(match.target);
+    var parent = match.target.parentElement;
     while(parent.dataset.matchid === undefined) {
       parent = parent.parentElement;
     }
---
+++ testcontent/factory/cult/__init__.py
@@ -0,0 +1,18 @@
+import factory
+from app import models, db
+from factory.fuzzy import FuzzyInteger, FuzzyChoice
+
+class CultStatFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = models.CultStats
+        sqlalchemy_session = db.session
+    runes_written = FuzzyInteger(0, 100)
+    runes_fumbled = FuzzyInteger(0, 100)
+    runes_nulled = FuzzyInteger(0, 100)
+    converted = FuzzyInteger(0, 100)
+    tomes_created = FuzzyInteger(0, 100)
+    narsie_summoned = FuzzyChoice(True, False)
+    if narsie_summoned:
+        narsie_corpses_fed = FuzzyInteger(0, 100)
+    surviving_cultists = FuzzyInteger(0, 100)
+    deconverted = FuzzyInteger(0, 100)
