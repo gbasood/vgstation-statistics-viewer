668668a24d716faf392857654e18f0ec80d2cc09
sood <gbasood@gmail.com>
2017-01-11 17:59:18 -0800

    Datelimit global stats (#12)
    * work
    
    * Monthly global stats
    
    * remove unused import

--- app/global_stats.py
+++ app/global_stats.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 from app import models, db, logging
+from app.helpers import add_months
+from sqlalchemy import and_
 from werkzeug.contrib.cache import SimpleCache
+import datetime
 import json
 
 cache = SimpleCache()
@@ -8,7 +11,6 @@ cache = SimpleCache()
 antag_objective_victory_modes = ["traitor+changeling", "double agents", "autotraitor", "changeling", "vampire", 'wizard', 'ragin\' mages', 'revolution']
 do_not_show = ['extended', 'heist', 'meteor']
 objective_success_threshold = 0.49
-
 
 class MatchTypeVictory:
     victory = False
@@ -26,9 +28,8 @@ class MatchTypeVictory:
     def __str__(self):
         return 'Mode: %s Victory: %s Secret: %s' % (self.mode, self.victory, self.secret)
 
-
-def get_formatted_global_stats():
-    stats = get_global_stats()
+def get_formatted_global_stats(timespan):
+    stats = get_global_stats(timespan)
 
     matchData = {}
     matchData['types'] = json.dumps(list(stats.keys()), ensure_ascii=True)
@@ -43,15 +44,20 @@ def get_formatted_global_stats():
     return matchData
 
 
-def get_global_stats():
+def get_global_stats(timespan):
 
     victories = dict()
     total = dict()
-    q = cache.get('globalstats')
+    cachestring = "globalstatsalltime"
+    if timespan[0]!="all":
+        cachestring = 'globalstats{}{}'.format(timespan[1].year, timespan[1].month)
+
+    q = cache.get(cachestring)
+
     if q is None:
         logging.debug('Cache miss on globalstats')
 
-        m = match_stats()
+        m = match_stats(timespan)
         for match in m:
             if match.mode not in victories:
                 victories[match.mode] = {'wins': 0, 'losses': 0}
@@ -62,15 +68,38 @@ def get_global_stats():
             else:
                 victories[match.mode]['losses'] = victories[match.mode]['losses'] + 1
 
-        cache.set('globalstats', victories, timeout=15 * 60)  # 15 minutes
+        cache.set(cachestring, victories, timeout=15 * 60)  # 15 minutes
     else:
         victories = q
         logging.debug('Cache hit on globalstats')
     return victories
 
 
-def match_stats():
-    q = models.Match.query.filter(models.Match.mastermode != "mixed" or '|' not in self.modes_string or 'meteor' not in models.Match.modes_string).all()
+def match_stats(timespan):
+    """
+    Returns all match victories/losses that match timespan.
+
+    Returns all matches that match timespan. Then checks the match's victory conditions in checkModeVictory.
+
+    Parameters
+    ----------
+    timespan : tuple
+        Three-part tuple. First part is "monthly" or "all". Second part is a datetime with month and year as the starting month.
+
+    Returns
+    -------
+    Array of MatchTypeVictory.
+
+    """
+    q = models.Match.query
+    if timespan[0] != "all": #TODO add browsing through dates that aren't  the current date
+        query_start = timespan[1]
+        query_end = add_months(query_start, 1)
+
+        q =q.filter(and_(models.Match.date is not None, models.Match.date >= query_start, models.Match.date < query_end))
+
+    q = q.filter(~models.Match.modes_string.contains('|'), ~models.Match.mastermode.contains('mixed'))
+    q = q.all()
 
     matches = []
 
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from app import app, parse, models, db, global_stats
+from app.helpers import add_months
+import datetime
 from config import MATCHES_PER_PAGE
 from flask import render_template, request
 import threading
@@ -34,8 +36,16 @@ def matchlist(page=1):
 
 # TODO: Use caching to store global stats results and only recalculate when needed, reducing load on server
 @app.route('/globalstats')
-def globalstats():
-    return render_template('globalstats.html', matchData=global_stats.get_formatted_global_stats())
+def globalstats(timespan="monthly", month=None, year=None):
+    query_timespan = request.args.get("timespan") if request.args.get("timespan") else "monthly"
+    request_month = int(request.args.get("month") if request.args.get("month") else datetime.datetime.now().month)
+    request_year = int(request.args.get("year") if request.args.get("year") else datetime.datetime.now().year)
+    request_starttime = datetime.datetime(year=request_year, month=request_month, day=1)
+
+    request_timespan = (query_timespan, request_starttime)
+    next_page = add_months(request_starttime, 1)
+    prev_page = add_months(request_starttime, -1)
+    return render_template('globalstats.html', matchData=global_stats.get_formatted_global_stats(request_timespan), timespan=query_timespan, query_start=request_starttime, nextpage = next_page, prevpage = prev_page)
 
 
 @app.route('/match/<id>')
