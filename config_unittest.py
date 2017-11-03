303085fcb47bbf38754a1b7c7c5cf30df5b05bd7
Sood <gbasood@gmail.com>
2017-11-03 09:17:56 -0700

    maybe now

--- config_unittest.py
+++ config_unittest.py
@@ -10,14 +10,15 @@ port = 5000
 
 # Path to stat files. Default value MUST be changed.
 STATS_DIR = os.path.join(basedir, 'test-statfiles')
-# This is where files get moved to once they're batch processed. They will never be used again and are kept around only for debugging the server code.
+# This is where files get moved to once they're batch processed.
+# They will never be used again and are kept around only for debugging the server code.
 PROCESSED_DIR = os.path.join(STATS_DIR, 'processed')
 # Files that could not be parsed.
 UNPARSABLE_DIR = os.path.join(STATS_DIR, 'unparsable')
 
 # Database
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'sqlite://'
+SQLALCHEMY_DATABASE_URI = os.path.join('sqlite://', basedir, 'tests', 'db', 'test.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = debug  # Track on debug
 
 # No longer used due to Manager implementation
